512588050c0815e62a4fa5c2a54a71382f10d47d
Paul Repka <repkap11@gmail.com>
2015-09-10 19:30:59 -0400

    Removed code generated by default project

---
+++ app/src/main/java/com/repkap11/chromecasturl/MainFragment.java
@@ -0,0 +1,21 @@
+package com.repkap11.chromecasturl;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        rootView.setKeepScreenOn(true);
+        return rootView;
+    }
+}
