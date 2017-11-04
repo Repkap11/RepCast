1e4686286b58ca42627773f0421b24b7b602715d
Paul Repka <repkap11@gmail.com>
2017-11-04 14:21:19 -0400

    Fix activity alias' not working
    Signed-off-by: Paul Repka <repkap11@gmail.com>

---
+++ app/src/main/java/com/repkap11/repcast/Launcher.java
@@ -0,0 +1,12 @@
+package com.repkap11.repcast;
+
+import android.app.Activity;
+
+import com.repkap11.repcast.activities.RepcastActivity;
+
+/**
+ * Created by paul on 11/4/17.
+ */
+
+public class Launcher extends RepcastActivity {
+}
